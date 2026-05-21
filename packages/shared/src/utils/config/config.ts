5f3b0ceaa311ebdf39042facb8cac1873d48373f
Your Name <you@example.com>
2026-05-21 10:03:52 +0300

    fix: keep app.deriv.com as 121856 while using 1069 for localhost

--- packages/shared/src/utils/config/config.ts
+++ packages/shared/src/utils/config/config.ts
@@ -16,7 +16,7 @@ export const livechat_client_id = '66aa088aad5a414484c1fd1fa8a5ace7';
 export const domain_app_ids = {
     // these domains as supported "production domains"
     'deriv.app': 16929, // TODO: [app-link-refactor] - Remove backwards compatibility for `deriv.app`
-    'app.deriv.com': 16929,
+    'app.deriv.com': 121856,
     'staging-app.deriv.com': 16303,
     'app.deriv.me': 1411,
     'staging-app.deriv.me': 1411, // TODO: setup staging for deriv.me
